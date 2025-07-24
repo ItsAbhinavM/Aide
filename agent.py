a471541bea77b645d7d63053a7ee74762ccd0196
itsabhinavm <abhinav051017@gmail.com>
2025-07-24 19:16:28 +0530

    feat: add share to discord

--- agent.py
+++ agent.py
@@ -3,7 +3,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage
 from langchain_tavily import TavilySearch
 from langchain.agents import initialize_agent, AgentType
-from langchain_core.tools import Tool
+from discord import send_to_discord
+from langchain_core.tools import Tool, tool
 load_dotenv()
 
 llm= ChatGoogleGenerativeAI(model='gemini-1.5-flash')
@@ -11,12 +12,24 @@ llm= ChatGoogleGenerativeAI(model='gemini-1.5-flash')
 # question=input("Enter your question : ")
 # reply=llm.invoke([HumanMessage(content=question)])
 
-search_tool= TavilySearch()
+search_tool= TavilySearch(max_results=1)
+
+@tool
+def send_to_discord_tool(message: str)-> str:
+    """Send a message through Discord."""
+    return send_to_discord(message)
+
 tools= [
     Tool(
         name="Tavily_search",
         func=search_tool.invoke,
         description="Search the web using Tavily for up-to-date information"
+    ),
+    Tool(
+        name="Send_to_discord",
+        func=send_to_discord_tool,
+        description="Send messages through discord",
+        return_direct=True
     )
 ]
 # search_result= search_tool.invoke("WHen is the next avengers movie")
@@ -24,7 +37,7 @@ tools= [
 agent= initialize_agent(
     tools=tools,
     llm=llm,
-    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, # does a reasoning before description
     verbose=True
 )
 
