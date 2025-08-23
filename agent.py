9531f4cf75dd25ad0c71a5ac0fad47245a7f0b2c
itsabhinavm <abhinav051017@gmail.com>
2025-08-23 16:14:10 +0530

    add system prompt

--- agent.py
+++ agent.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from langchain_tavily import TavilySearch
 from langchain.agents import initialize_agent, AgentType
 from Components.discord import send_to_discord
@@ -8,7 +10,37 @@ from Components.youtube import youtube_loader
 from langchain_core.tools import Tool, tool
 load_dotenv()
 
-llm= ChatGoogleGenerativeAI(model='gemini-1.5-flash')
+system_prompt_template = """
+You are an orchestrator Agent and a reliable personal assistant.
+Your primary role is to coordinate tools and provide clear, structured and helpful response.
+
+Available capabilities:
+1. **Write detailed notes**  
+   - Convert raw text or transcripts into well-organized notes.  
+   - Always include main topics, subtopics, key points, and a short summary.  
+2. **YouTube Transcript + Inference**  
+   - If the user provides a valid YouTube link, fetch the transcript.  
+   - Then create structured notes or a clear summary of the video’s content.  
+
+3. **Discord Messaging**  
+   - If the user explicitly asks you to send a message to Discord, use the Discord tool.  
+   - Confirm with the user before sending sensitive content.
+4. **Object detection**
+    - If the user explicity asks for whats there infront of him, use the Object detector tool.
+    - Describe the detected objects clearly, including positions or counts only if relevant.
+
+General rules:
+- Always decide the best tool to use based on the user's request.
+- If multiple tools are needed, (e.g., YouTube transcript → detailed notes), orchestrate them step by step. 
+- If the request is outside the capabilities, politely explain your limitations.
+- Ensure responses are professional, consise and actionable.
+"""
+
+system_prompt= ChatPromptTemplate.from_messages([
+    ("system",system_prompt_template),
+    ("human","{input}")
+])
+
 search_tool= TavilySearch(max_results=1)
 
 @tool
@@ -71,9 +103,12 @@ tools= [
 ]
 # search_result= search_tool.invoke("WHen is the next avengers movie")
 
+llm= ChatGoogleGenerativeAI(model='gemini-1.5-flash',system_instruction=system_prompt_template)
+orchestrator_chain= system_prompt | llm | StrOutputParser()
+
 agent= initialize_agent(
     tools=tools,
-    llm=llm,
+    llm=orchestrator_chain,
     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, # does a reasoning before description
     verbose=True,
     handle_parsing_errors=True
