662ba2555a320a0493b81faee263f8b689d63c49
itsabhinavm <abhinav051017@gmail.com>
2025-10-01 23:37:38 +0530

    feat: update LandingPage UI and structure

--- aide/src/Pages/LandingPage.tsx
+++ aide/src/Pages/LandingPage.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import PixelBlast from "../Components/dotGrid";
 
 export default function LandingPage() {
     const navigate= useNavigate()
@@ -6,15 +7,38 @@ export default function LandingPage() {
         navigate('/chat')
         console.log("Routed to ChatPage");
     }
+    const handleLearnMore = () => {
+        window.__TAURI__.shell.open("https://github.com/itsabhinavm/aide");
+        console.log("Learn More button clicked")
+    }
 
     return (
         <div className="flex-1 w-full h-screen flex items-center justify-center">
+            <PixelBlast
+                variant="circle"
+                pixelSize={6}
+                color="#B19EEF"
+                patternScale={3}
+                patternDensity={1.2}
+                pixelSizeJitter={0.5}
+                enableRipples
+                rippleSpeed={0.4}
+                rippleThickness={0.12}
+                rippleIntensityScale={1.5}
+                liquid
+                liquidStrength={0.12}
+                liquidRadius={1.2}
+                liquidWobbleSpeed={5}
+                speed={0.6}
+                edgeFade={0.25}
+                transparent
+            />
             <div className="text-center space-y-6">
-                <h1 className="text-5xl font-extrabold text-white leading-tight animate-pulse">Personalized Orchestration,<br /> Now Easier!</h1>
+                <h1 className="text-5xl font-extrabold text-white leading-tight ">Personalized Orchestration,<br /> Now Easier!</h1>
 
                 <div className="flex items-center justify-center space-x-4 pt-4">
                     <button className="px-6 py-3 rounded-full bg-white text-black font-medium shadow-md hover:bg-gray-200 transition" onClick={handleGoToChat}>Get Started</button>
-                    <button className="px-6 py-3 rounded-full bg-white text-black font-medium shadow-md hover:bg-gray-200 transition">Learn More</button>
+                    <button className="px-6 py-3 rounded-full border border-white text-white font-medium shadow-md hover:bg-white/10 transition" onClick={handleLearnMore}>Learn More</button>
                 </div>
             </div>
         </div>
